62c91d6495f54a79f97bf38a65a90e7c2476f090
Sebastian Sciarra <sebastiansciarra@Sebastians-MacBook-Air-2.local>
2023-03-09 23:10:51 -0500

    march 9

--- public/js/mathjax_config.js
+++ public/js/mathjax_config.js
@@ -1,18 +1,10 @@
 
 //see https://github.com/mathjax/MathJax/issues/3013
 window.MathJax = {
-   options: {
-    enableMenu: true,          // set to false to disable the menu
-    menuOptions: {
-      settings: {
-        displayOverflow: 'linebreak'
-        }
-      }
-   },
   section: {
     n: -1,
     useLetters: false,
-    letters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letters: "AABCDEFGHIJKLMNOPQRSTUVWXYZ"
   },
 
   loader: {load: ['[tex]/tagformat', '[tex]/mathtools', 'output/chtml']},
@@ -38,7 +30,7 @@ window.MathJax = {
   linebreaks: {                  // options for when overflow is linebreak
       inline: true,                   // true for browser-based breaking of inline equations
       width: '100%',                  // a fixed size or a percentage of the container width
-      lineleading: .2,                // the default lineleading in em units
+      lineleading: 2,                // the default lineleading in em units
       LinebreakVisitor: null,         // The LinebreakVisitor to use
   },
 
@@ -46,6 +38,9 @@ window.MathJax = {
 
   startup: {
     ready() {
+                  const {CommonWrapper} = MathJax._.output.common.Wrapper;
+      const {LineBBox} = MathJax._.output.common.LineBBox;
+
       const Configuration = MathJax._.input.tex.Configuration.Configuration;
       const CommandMap = MathJax._.input.tex.SymbolMap.CommandMap;
       new CommandMap('sections', {
@@ -67,8 +62,29 @@ window.MathJax = {
             section.n = parseInt(c);
             section.useLetters = false;
           }
+        },
+      });
+      Object.assign(CommonWrapper.prototype, {
+        invalidateBBox(bubble = true) {
+          if (this.bboxComputed || this._breakCount >= 0) {
+            this.bboxComputed = false;
+            this.lineBBox = [];
+            this._breakCount = -1;
+            if (this.parent && bubble) {
+              this.parent.invalidateBBox();
+            }
+          }
+        },
+        _getLineBBox: CommonWrapper.prototype.getLineBBox,
+        getLineBBox(i) {
+          if (!this.lineBBox[i] && !this.breakCount) {
+            const obox = this.getOuterBBox();
+            this.lineBBox[i] = LineBBox.from(obox, this.linebreakOptions.lineleading);
+          }
+          return this._getLineBBox(i);
         }
       });
+
       Configuration.create(
         'sections', {handler: {macro: ['sections']}}
       );
